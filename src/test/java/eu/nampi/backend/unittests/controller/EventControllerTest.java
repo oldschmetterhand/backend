6e191b0c4e8c87d0ba0b4c163fef0bc1d01332ac
Sebi <SebastianStoff@gmx.at>
2020-01-22 15:47:38 +0100

    added uniittests against the eventController

---
+++ src/test/java/eu/nampi/backend/unittests/controller/EventControllerTest.java
@@ -0,0 +1,52 @@
+package eu.nampi.backend.unittests.controller;
+
+import eu.nampi.backend.controller.EventController;
+import eu.nampi.backend.repository.EventRepository;
+import eu.nampi.backend.util.JenaUtils;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.riot.Lang;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import javax.servlet.http.HttpServletResponse;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
+
+@DisplayName("EventController Unittests")
+@ExtendWith(MockitoExtension.class)
+public class EventControllerTest {
+
+    @Mock
+    EventRepository eventRepository;
+
+    @Mock
+    JenaUtils jenaUtils;
+
+    @Mock
+    Model model;
+
+    @Mock
+    Lang jenaLang;
+
+    @Mock
+    HttpServletResponse httpServletResponse;
+
+    @InjectMocks
+    EventController eventController;
+
+    @Test
+    @DisplayName("getEvents should lead to jenaUtils try to write given model into jena.")
+    void testGetEvents() {
+        //given
+        given(eventRepository.getEvents()).willReturn(model);
+        // when
+        eventController.getEvents(jenaLang, httpServletResponse);
+        //then
+        then(jenaUtils).should(times(1)).writeModel(model, jenaLang, httpServletResponse);
+    }
+
+}
